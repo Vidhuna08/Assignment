22e06fefc39a3b377cd5f290663abc9491effd46
Vidhuna08 <112848233+Vidhuna08@users.noreply.github.com>
2022-11-17 22:10:37 +0530

    Add files via upload

---
+++ week1/day1/Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+		
+		for(int index =0;1;index++)
+			
+		{
+			System.out.println("Welcome");
+			break;
+		}
+	}
+
+}
